7a213e6e965cda7d09f3a3bbd98c53aa95203945
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-03-26 21:07:41 -0700

    chloropleth is not what is needed turns out

--- Leaflet-Step1/js/logic.js
+++ Leaflet-Step1/js/logic.js
@@ -6,11 +6,13 @@ var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.g
 
 d3.json(url, function(data) {
 
+    console.log(data)
+
     // Once we get a response, send the data.features object to the createFeatures function
 
     createFeatures(data.features);
 
-  });
+})
   
 function createFeatures(earthquakeData) {
 
